f95cf65feae5b5ddcc519a13e3f459355c259df7
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2025-01-21 20:00:13 +0300

    Мои добавления в user.js

--- user.js
+++ user.js
@@ -223,11 +223,52 @@ user_pref("layout.word_select.eat_space_to_next_word", false);
 // visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
 // Enter your personal overrides below this line:
 
+// PREF: download to Downloads automatically
+user_pref("browser.download.useDownloadDir", true);
+
+// PREF: dark theme
+user_pref("browser.theme.content-theme", 0);
+user_pref("browser.theme.toolbar-theme", 0);
+
+// PREF: return Compact icons
+user_pref("browser.uidensity", 1);
+
+// PREF: UI
+user_pref("browser.download.autohideButton", false); // don't hide download button
+user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"unified-extensions-area\":[\"ublock0_raymondhill_net-browser-action\",\"_36bdf805-c6f2-4f41-94d2-9b646342c1dc_-browser-action\",\"copyplaintext_eros_man-browser-action\",\"_74145f27-f039-47ce-a470-a662b129930a_-browser-action\",\"_c2c003ee-bd69-42a2-b0e9-6f34222cb046_-browser-action\",\"browsec_browsec_com-browser-action\",\"_1935b298-ac62-11ec-b909-0242ac120002_-browser-action\",\"light_plugin_7571494ce0b94e11bb762b659a4ad71f_kaspersky_com-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"customizableui-special-spring1\",\"urlbar-container\",\"customizableui-special-spring2\",\"save-to-pocket-button\",\"downloads-button\",\"bookmarks-menu-button\",\"history-panelmenu\",\"keepassxc-browser_keepassxc_org-browser-action\",\"button_scholar_google_com-browser-action\",\"zotero_chnm_gmu_edu-browser-action\",\"_f209234a-76f0-4735-9920-eb62507a54cd_-browser-action\",\"unified-extensions-button\",\"reset-pbm-toolbar-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"firefox-view-button\",\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"ublock0_raymondhill_net-browser-action\",\"keepassxc-browser_keepassxc_org-browser-action\",\"_36bdf805-c6f2-4f41-94d2-9b646342c1dc_-browser-action\",\"_f209234a-76f0-4735-9920-eb62507a54cd_-browser-action\",\"copyplaintext_eros_man-browser-action\",\"_74145f27-f039-47ce-a470-a662b129930a_-browser-action\",\"_c2c003ee-bd69-42a2-b0e9-6f34222cb046_-browser-action\",\"browsec_browsec_com-browser-action\",\"_1935b298-ac62-11ec-b909-0242ac120002_-browser-action\",\"zotero_chnm_gmu_edu-browser-action\",\"light_plugin_7571494ce0b94e11bb762b659a4ad71f_kaspersky_com-browser-action\",\"button_scholar_google_com-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"PersonalToolbar\",\"unified-extensions-area\",\"toolbar-menubar\",\"TabsToolbar\"],\"currentVersion\":20,\"newElementCount\":7}");
+
+// PREF: don't close Firefox with the last tab
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+
+// PREF: switch recent tabs by CTRL+TAB
+user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
+
+// PREF: restore Top Sites on New Tab page
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", true); // Shortcuts
+user_pref("browser.newtabpage.activity-stream.default.sites", ""); // clear default topsites
+
+// PREF: remove sponsored content on New Tab page
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false); // Sponsored shortcuts
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false); // Recommended by Pocket
+user_pref("browser.newtabpage.activity-stream.showSponsored", false); // Sponsored Stories
+
+// PREF: no search on New Tab page
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+
+// PREF: restore search engine suggestions
+user_pref("browser.search.suggest.enabled", true);
+
 /****************************************************************************
  * SECTION: SMOOTHFOX                                                       *
 ****************************************************************************/
 // visit https://github.com/yokoffing/Betterfox/blob/main/Smoothfox.js
 // Enter your scrolling overrides below this line:
+
+// recommended for 90hz+ displays
+user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+user_pref("general.smoothScroll", true); // DEFAULT
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
 
 /****************************************************************************
  * END: BETTERFOX                                                           *
